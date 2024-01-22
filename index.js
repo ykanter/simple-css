30bbf771a38b80394a67df0925639e8b02fc3012
Yakov Kanter <ykanter@gmail.com>
2024-01-21 20:41:27 -0500

    remove the port number

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = Bun.serve(
     {
-        port : 3000,
+
         fetch(req) {
             const url = new URL(req.url)
             if(url.pathname.endsWith("/") || url.pathname.endsWith("/index.html")){
